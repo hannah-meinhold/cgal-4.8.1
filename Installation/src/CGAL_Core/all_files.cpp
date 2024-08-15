17454d5ca33698fcbb5c5a60b4241d962a710c34
Hannah Meinhold <hannah.meinhold@rwth-aachen.de>
2024-08-15 17:11:34 +0200

    some more changes, cmake does run now

---
+++ Installation/src/CGAL_Core/all_files.cpp
@@ -0,0 +1,8 @@
+#include "//home/hannah/Dokumente/medial-axis/Coverage_Axis/skel_connection/extern/cgal/CGAL_Core/src/CGAL_Core/BigFloat.cpp"
+#include "//home/hannah/Dokumente/medial-axis/Coverage_Axis/skel_connection/extern/cgal/CGAL_Core/src/CGAL_Core/CoreAux.cpp"
+#include "//home/hannah/Dokumente/medial-axis/Coverage_Axis/skel_connection/extern/cgal/CGAL_Core/src/CGAL_Core/CoreDefs.cpp"
+#include "//home/hannah/Dokumente/medial-axis/Coverage_Axis/skel_connection/extern/cgal/CGAL_Core/src/CGAL_Core/CoreIO.cpp"
+#include "//home/hannah/Dokumente/medial-axis/Coverage_Axis/skel_connection/extern/cgal/CGAL_Core/src/CGAL_Core/Expr.cpp"
+#include "//home/hannah/Dokumente/medial-axis/Coverage_Axis/skel_connection/extern/cgal/CGAL_Core/src/CGAL_Core/GmpIO.cpp"
+#include "//home/hannah/Dokumente/medial-axis/Coverage_Axis/skel_connection/extern/cgal/CGAL_Core/src/CGAL_Core/Real.cpp"
+#include "//home/hannah/Dokumente/medial-axis/Coverage_Axis/skel_connection/extern/cgal/CGAL_Core/src/CGAL_Core/extLong.cpp"
